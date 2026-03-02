ea0e68309480ee1e35352d050f18ca7e3ef3224e
Kinderu <jahnanteksp13@gmail.com>
2026-03-02 15:20:58 +0100

    update

---
+++ cetuspro0203/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppTest.Entities;
+
+namespace WebAppTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetVars()
+        {
+            var vars = new List<Calculator> { 
+                new Calculator {A=Random.Shared.Next(-1000, 1000),B=Random.Shared.Next(-1000, 1000) }
+            };
+            return Ok(vars);
+        }
+    }
+}
---
+++ cetuspro0203/Entities/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace WebAppTest.Entities
+{
+    public class Calculator
+    {
+        public double A { set; get; }
+        public double B { set; get; }
+    }
+}
