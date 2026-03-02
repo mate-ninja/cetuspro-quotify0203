dcab7a9c84ca75a5b421ec9270ff964ab07f9b38
Mate Ninja <mateusz.obi23@gmail.com>
2026-03-02 14:52:39 +0100

    Dodaj pliki projektów.

---
+++ cetuspro0203/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using cetuspro0203.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace cetuspro0203.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
