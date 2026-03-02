76d61c9acd43470c208d6cab33c99ab2824c4f85
Kinderu <jahnanteksp13@gmail.com>
2026-03-02 16:16:35 +0100

    API added

---
+++ cetuspro0203/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using cetuspro0203.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cetuspro0203.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetVars()
+        {
+            var vars = new List<Calculator> {
+                new Calculator {A=Random.Shared.Next(-1000, 1000),B=Random.Shared.Next(-1000, 1000) }
+            };
+            return Ok(vars);
+        }
+    }
+}
---
+++ cetuspro0203/Entites/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace cetuspro0203.Entites
+{
+    public class Calculator
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+}
