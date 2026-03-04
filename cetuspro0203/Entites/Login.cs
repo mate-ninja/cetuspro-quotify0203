c58a538858410e4220367981df339b314031c181
Mate Ninja <mateusz.obi23@gmail.com>
2026-03-04 11:15:09 +0100

    Revert "added things for user table in database"
    This reverts commit e4bc2e35931f0eaed5eef611a8e3a51af5b4800d.

--- cetuspro0203/Entites/Login.cs
+++ cetuspro0203/Entites/Login.cs
@@ -4,7 +4,5 @@
     {
         public required string Username { get; set; }
         public required string Password { get; set; }
-        public required string Email { get; set; }
-        public required Enum Admin { get; set; }
     }
 }
